c4ad4ec9acba10ad046257a8971e3c9876fd1d70
Dawid Rashid <davo22@users.noreply.github.com>
2018-06-11 13:24:04 +0200

    feat(*): data product purchase (#4)

--- repux-web3-api.d.ts
+++ repux-web3-api.d.ts
@@ -1,3 +1,47 @@
+import BigNumber from 'bignumber.js';
+
+export enum DataProductUpdateAction {
+    CREATE = '0',
+    UPDATE = '1',
+    DELETE = '2',
+    PURCHASE = '3',
+    APPROVE = '4',
+    RATE = '5',
+    CANCEL_RATING = '6'
+}
+
+export interface DataProductEvent {
+    dataProductAddress: string;
+    userAddress: string;
+    dataProductUpdateAction: DataProductUpdateAction;
+    blockNumber: number;
+}
+
+export interface TransactionResult {
+    address: string;
+    blockNumber: string;
+    blockHash: string;
+    status: string;
+}
+
+export interface DataProductTransaction {
+    publicKey: string;
+    buyerMetaHash: string;
+    price: BigNumber;
+    purchased: boolean;
+    approved: boolean;
+    rated: boolean;
+    rating: BigNumber;
+}
+
+export interface DataProduct {
+    address: string;
+    owner: string;
+    price: BigNumber;
+    sellerMetaHash: string;
+    totalRating: BigNumber;
+}
+
 export default class RepuxWeb3Api {
     private web3;
     private registryContractAddress;
@@ -16,27 +60,81 @@ export default class RepuxWeb3Api {
      * Return API version
      * @returns {string} API version
      */
-    getVersion(): any;
+    getVersion(): string;
 
     /**
      * Returns default account
      * @returns {string} Default account
      */
-    getDefaultAccount(): any;
+    getDefaultAccount(): string;
 
     /**
      * Return account balance value
      * @param {string} [account=web3.eth.defaultAccount] - Account address
      * @returns {Promise<BigNumber>} Balance value
      */
-    getBalance(account?: string): Promise<any>;
+    getBalance(account?: string): Promise<BigNumber>;
 
     /**
      * Creates product contract
-     * @param {string} metaFileHash - Hash of meta file containing all data required to product publication
-     * @param {BigNumber} price - Product price
-     * @param {string} [account=web3.eth.defaultAccount] - Account address
-     * @returns {Promise<*>}
+     * @param metaFileHash
+     * @param price
+     * @param account
      */
-    createDataProduct(metaFileHash: string, price: any, account: string): Promise<any>;
+    createDataProduct(metaFileHash: string, price: BigNumber, account: string): Promise<TransactionResult>;
+
+    /**
+     * Watches for DataProductUpdate events on blockchain
+     * @param config
+     * @param callback
+     */
+    watchForDataProductUpdate(config: { fromBlock: number, toBlock: number | string }, callback: (DataProductEvent) => void);
+
+    /**
+     * Returns DataProduct data
+     * @param dataProductAddress
+     */
+    getDataProduct(dataProductAddress: string): Promise<DataProduct>
+
+    /**
+     * Purchases DataProduct
+     * @param dataProductAddress
+     * @param publicKey
+     * @param account
+     */
+    purchaseDataProduct(dataProductAddress: string, publicKey: string, account?: string): Promise<TransactionResult>
+
+    /**
+     * Approves DataProduct purchase
+     * @param {string} dataProductAddress
+     * @param {string} buyerAddress
+     * @param {string} buyerMetaHash
+     * @param {string} account
+     */
+    approveDataProductPurchase(dataProductAddress: string, buyerAddress: string, buyerMetaHash: string, account?: string): Promise<TransactionResult>
+
+    /**
+     * Returns products bought by provided account
+     * @param account
+     */
+    getBoughtDataProducts(account?: string): Promise<string[]>
+
+    /**
+     * Returns products bought by provided account and approved
+     * @param account
+     */
+    getBoughtAndApprovedDataProducts(account?: string): Promise<string[]>
+
+    /**
+     * Returns products created by provided account
+     * @param account
+     */
+    getCreatedDataProducts(account?: string): Promise<string[]>
+
+    /**
+     * Returns transaction on DataProduct by buyerAddress
+     * @param dataProductAddress
+     * @param buyerAddress
+     */
+    getDataProductTransaction(dataProductAddress: string, buyerAddress: string): Promise<DataProductTransaction>
 }
